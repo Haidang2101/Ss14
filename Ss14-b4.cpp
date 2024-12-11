8135421ac2ffd6e70cfdce6cdeacbb62cecc1480
Haidang2101 <haidanghanoi2003@gmail.com>
2024-12-11 22:58:35 +0700

    b4

---
+++ Ss14-b4.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    char str[] = "Hello,bro do you want to play the game?"; 
+    char ch;
+    int count = 0;
+    printf("Nhap mot ky tu: ");
+    scanf("%c", &ch);
+    for (int i = 0; str[i] != '\0'; i++) {
+        if (str[i] == ch) {
+            count++;
+        }
+    }
+    printf("Ky tu '%c' xuat hien %d lan trong chuoi.\n", ch, count);
+    
+    return 0;
+}
+
