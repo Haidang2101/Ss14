a9ac6f7bad20aa80fda06956d76062fc8f17a6d0
Haidang2101 <haidanghanoi2003@gmail.com>
2024-12-12 15:45:51 +0700

    Update ss14-b3.cpp

--- ss14-b3.cpp
+++ ss14-b3.cpp
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main() {
-    char str[] = "World Hello "; 
+    char str[] = "ytuD fO llaC"; 
     for (int i = sizeof(str)-1; i >= 0; i--) {
         printf("%c ", str[i]);  
     }
